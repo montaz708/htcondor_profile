30ca77f536acda8810091dcc0201ae2605b2d69b
Alex Vazquez <alex.vazquez708@gmail.com>
2019-04-11 14:34:52 -0400

    Changing get_condor_from_repo so that it builds from the Ubuntu16 repo

--- get_condor_from_repo.sh
+++ get_condor_from_repo.sh
@@ -2,8 +2,8 @@
 set -x
 wget -qO - https://research.cs.wisc.edu/htcondor/ubuntu/HTCondor-Release.gpg.key | sudo apt-key add -
 # Do this as root
-echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic bionic contrib" >> /etc/apt/sources.list
-echo "deb-src http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic bionic contrib" >> /etc/apt/sources.list
+echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/8.8/xenial xenial contrib" >> /etc/apt/sources.list
+echo "deb-src http://research.cs.wisc.edu/htcondor/ubuntu/8.8/xenial xenial contrib" >> /etc/apt/sources.list
 apt-get update -y
 apt-get install -y htcondor
 systemctl start condor
